263f092474d908e7d94fd2a9e1fdc1b21d13d366
mbumare <68236407+mbumare@users.noreply.github.com>
2020-07-14 19:54:32 +0530

    Add files via upload

---
+++ Swapping.py
@@ -0,0 +1,25 @@
+# Swapping Using Arithmatic Operetor.
+a= 5
+b= 10
+
+a=a+b
+b=a-b
+a=a-b
+print('values of a and b after swapping')
+print(a)
+print(b)
+
+# Swapping Using Temp Variable
+
+a=5
+b=10
+
+c=a
+a=b
+b=c
+print('values of a and b after swapping')
+print(a)
+print(b)
+
+ 
+
